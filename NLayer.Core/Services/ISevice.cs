3ed7f22026ee2d600daf50f08c43d70a7b9b105b
Admin <Admin@DESKTOP-N0M3JFJ>
2022-07-12 09:10:51 +0300

    Add project files.

---
+++ NLayer.Core/Services/ISevice.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Services
+{
+    public interface ISevice<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GelAllAsync();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entiity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
+ 
---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> productFeatures { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
